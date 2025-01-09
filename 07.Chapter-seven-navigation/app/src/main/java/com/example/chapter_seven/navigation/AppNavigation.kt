f7105e43be60a382eb373caba74397dab38d3011
Ovidijus <ovidijus.e@gmail.com>
2025-01-09 10:43:28 +0200

    Fix navigation

--- 07.Chapter-seven-navigation/app/src/main/java/com/example/chapter_seven/navigation/AppNavigation.kt
+++ 07.Chapter-seven-navigation/app/src/main/java/com/example/chapter_seven/navigation/AppNavigation.kt
@@ -1,14 +1,41 @@
 package com.example.chapter_seven.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.chapter_seven.data.Cat
 import com.example.chapter_seven.views.PetDetailsScreen
 import com.example.chapter_seven.views.PetsScreen
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.net.URLDecoder
+import java.net.URLEncoder
 
+
+//@Composable
+//fun AppNavigation() {
+//    val navController = rememberNavController()
+//    NavHost(navController = navController, startDestination = Screens.PetsScreen.route) {
+//        composable(Screens.PetsScreen.route) {
+//            PetsScreen(onPetClicked = { cat ->
+//               navController.navigate("${Screens.PetDetailScreen.route}/${Json.encodeToString(cat)}")
+//            })
+//        }
+//        composable(route = "${Screens.PetDetailScreen.route}/{catId}",
+//            arguments = listOf(navArgument("catId") {
+//                type = NavType.StringType
+//            }
+//            )) {
+//            PetDetailsScreen(
+//                onBackPress = { navController.popBackStack() },
+//                cat = Json.decodeFromString(it.arguments?.getString("cat") ?: ""),
+//            )
+//        }
+//    }
+//}
 
 @Composable
 fun AppNavigation() {
@@ -16,13 +43,24 @@ fun AppNavigation() {
     NavHost(navController = navController, startDestination = Screens.PetsScreen.route) {
         composable(Screens.PetsScreen.route) {
             PetsScreen(onPetClicked = { cat ->
-                navController.navigate(Screens.PetDetailScreen.route)
+                val encodedCat = URLEncoder.encode(Json.encodeToString(cat), "UTF-8")
+                navController.navigate("${Screens.PetDetailScreen.route}/$encodedCat")
             })
         }
-        composable("${Screens.PetDetailScreen.route}/{cat}") {
+        composable(
+            route = "${Screens.PetDetailScreen.route}/{catId}",
+            arguments = listOf(
+                navArgument("catId") {
+                    type = NavType.StringType
+                }
+            )
+        ) { backStackEntry ->
+            val encodedCat = backStackEntry.arguments?.getString("catId") ?: ""
+            val decodedCat = URLDecoder.decode(encodedCat, "UTF-8")
+            val cat = Json.decodeFromString<Cat>(decodedCat)
             PetDetailsScreen(
                 onBackPress = { navController.popBackStack() },
-                cat = Json.decodeFromString(it.arguments?.getString("cat") ?: ""),
+                cat = cat
             )
         }
     }
